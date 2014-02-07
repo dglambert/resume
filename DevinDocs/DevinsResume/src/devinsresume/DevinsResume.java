192ec46eed41ea865d950b1dc571441e3a72b953
dglambert <dglambert@outlook.com>
2014-02-07 09:39:57 -0500

    initial commit

---
+++ DevinDocs/DevinsResume/src/devinsresume/DevinsResume.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devinsresume;
+
+
+/**
+ *
+ * @author Devin
+ */
+public class DevinsResume
+{
+    public String education;
+
+    public DevinsResume()
+    {}
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main( String[] args )
+    {
+        // TODO code application logic here
+    }
+
+    public void education()
+    {}
+
+    public void career()
+    {}
+
+    public void systemsExperience()
+    {}
+
+    public void collegeActivities()
+    {}
+
+
+
+}
